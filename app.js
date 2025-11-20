8c0a35871fba612671d45dcb71363d66ec69b01c
Ankitrwat2004 <ankitrawat3700@gmail.com>
2025-11-21 02:31:12 +0530

    add some changes

--- app.js
+++ app.js
@@ -118,9 +118,12 @@ app.use((err,req,res,next)=>{
   // res.status(statusCode).send(message);
 }) ;
 
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
 });
+
 
 //express router => are a way to organize your express application such thtat our primary app.js file does not bloated.
 //const router = express.Router() [create new router object]
